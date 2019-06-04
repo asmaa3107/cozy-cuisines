fcaa1ce9997931c4a84e3d1bc5729954c2eb51e2
asmaa3107 <asmaa3107@gmail.com>
2019-06-04 15:59:03 +0200

    edit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+const _isDev = window.location.port.indexOf('4200') > -1;
+const getHost = () => {
+  const protocol = window.location.protocol;
+  const host = window.location.host;
+  return `${protocol}//${host}`;
+};
+const apiURI = _isDev ? 'http://localhost:8083/api/' : `/api/`;
+
 export const environment = {
-  production: true
+
+  production: false,
+  apiKey: "51c4d4824dbf0cd82bbf67911cfe7c14",
+  apiUrl: "https://developers.zomato.com/api/v2.1/cuisines?city_id=280&",
+  BASE_URI: getHost(),
+  BASE_API: apiURI
+ 
 };
